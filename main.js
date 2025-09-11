1f95dccb342642c546b594ea68223374e571541d
pongpoti <pong.poti@gmail.com>
2025-09-11 14:37:57 +0700

    c

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-import express from "npm:express";
-import process from "node:process";
+import express from "npm:express"
+import process from "node:process"
+import path from "node:path"
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
+const __dirname = import.meta.dirname
 
 app.listen(port, () => {
-    console.log(`listening on ${port}`);
-});
+    console.log(`listening on ${port}`)
+})
 
-app.use("/daisyui", express.static("daisyui"));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use("/completeupload", express.static("completeupload"))
