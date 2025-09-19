d70b439613e44d1fe9670bf3c7b9990d5e6c9e7d
pongpoti <pong.poti@gmail.com>
2025-09-19 22:30:17 +0700

    c

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
-import express from "npm:express"
-import process from "node:process"
-import path from "node:path"
+import express from "npm:express";
+import process from "node:process";
+import path from "node:path";
+import { createClient } from "npm:@supabase/supabase-js@2";
 
 const app = express();
-const port = process.env.PORT || 3000
-const __dirname = import.meta.dirname
+const port = process.env.PORT || 3000;
+const __dirname = import.meta.dirname;
+const supabaseUrl = Deno.env.get("supabaseUrl");
+const supabaseKey = Deno.env.get("supabaseKey");
+const supabase = createClient(supabaseUrl, supabaseKey);
+const { data, error } = await supabase
+    .from("src")
+    .select()
 
 app.listen(port, () => {
-    console.log(`listening on ${port}`)
-})
+    console.log(`listening on ${port}`);
+});
 
-app.use(express.static(path.join(__dirname, "main")))
-app.use("/completeupload", express.static("completeupload"))
-app.use("/logout", express.static("logout"))
-app.use("/verify", express.static("verify"))
+app.use(express.static(path.join(__dirname, "main")));
+app.use("/completeupload", express.static("completeupload"));
+app.use("/logout", express.static("logout"));
+app.use("/verify", express.static("verify"));
 
 app.get("/sapi", (req, res) => {
-    console.log(Deno.env.get("supabaseUrl"))
-    console.log(Deno.env.get("supabaseKey"))
-})
+    console.log(data);
+    res.json(data);
+});
